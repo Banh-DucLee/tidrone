0f734c14a4a39b87d1720fdc33195e8c6a7311e4
Pekaizilla <duclee.banh@gmail.com>
2024-12-16 16:40:53 +0100

    Add typo & shadow as global css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: [
+    'assets/css/typography.css',
+    'assets/css/shadow.css'
+  ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
