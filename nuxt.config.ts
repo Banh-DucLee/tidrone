703efc168ff4f34fc12a01e1a00a26cc0010cab6
Pekaizilla <duclee.banh@gmail.com>
2024-12-16 19:20:41 +0100

    Added variables.css to global css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   css: [
     'assets/css/typography.css',
-    'assets/css/shadow.css'
+    'assets/css/shadow.css',
+    'assets/css/variables.css'
   ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
