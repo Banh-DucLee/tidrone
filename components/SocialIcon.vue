941512f0397cc9707de4a23d521a0a86f503d111
Pekaizilla <duclee.banh@gmail.com>
2024-12-17 14:53:59 +0100

    Created SocialIcon component

---
+++ components/SocialIcon.vue
@@ -0,0 +1,37 @@
+<template>
+    <a :href="link" target="_blank" :alt="'Lien vers ' + socialName" class="social-icon-link">
+        <img :src="svgUrl" :alt="'Logo de ' + socialName" class="social-icon">
+    </a>
+</template>
+
+<script setup>
+defineProps({
+    svgUrl: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: true,
+    },
+    socialName: {
+        type: String,
+        required: true,
+    }
+});
+</script>
+
+<style scoped>
+.social-icon-link {
+    display: block;
+    aspect-ratio: 1;
+    width: 50px;
+    height: auto;
+}
+
+.social-icon {
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+}
+</style>
