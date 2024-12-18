4bbf4fb3c866a05cf75ccfe131b27426e9c3f982
Pekaizilla <duclee.banh@gmail.com>
2024-12-18 21:34:21 +0100

    Renamed prop buttonStyle to buttonClass in Button component

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,6 @@
 <template>
-    <NuxtLink v-if="buttonType === 'redirect'" class="button-component text-center"  :to="to" :class="buttonStyle">{{ text }}</NuxtLink>
-    <a v-else-if="buttonType === 'mail'" class="button-component text-center"  :href="`mailto:${to}`" :class="buttonStyle" target="_blank">{{ text }}</a>
+    <NuxtLink v-if="buttonType === 'redirect'" class="button-component text-center"  :to="to" :class="buttonClass">{{ text }}</NuxtLink>
+    <a v-else-if="buttonType === 'mail'" class="button-component text-center"  :href="`mailto:${to}`" :class="buttonClass" target="_blank">{{ text }}</a>
 </template>
 
 <script setup>
@@ -18,7 +18,7 @@ defineProps({
         type: String,
         required: true,
     },
-    buttonStyle: {
+    buttonClass: {
         type: String,
         required: true,
     }
@@ -28,13 +28,28 @@ defineProps({
 <style scoped>
     .button-component {
         text-decoration: none;
-        color: var(--color-main-black);
-        border: 2px solid var(--color-main-black);
-        padding: 5px 10px;
+        color: var(--color-main-white);
+        background-color: var(--color-primary-500);
+        padding: 12px 16px;
+        border-radius: 4px;
+        text-shadow: 0px 0px 4px var(--color-opacity-black-25);
     }
 
     .button-component:hover {
-        color: var(--color-primary-800);
-        border-color: var(--color-primary-800);
+        background-color: var(--color-primary-700);
     }
+
+    .button-component:active {
+        background-color: var(--color-primary-800);
+    }
+
+    .button-component:disabled {
+        color: var(--color-opacity-black-50);
+        background-color: var(--color-opacity-black-25);
+    }
+
+    .button-component:focus {
+        outline-color: var(--color-primary-800);
+    }
+
 </style>
