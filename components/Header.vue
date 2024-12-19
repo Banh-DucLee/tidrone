a13e7f986ff9c1eace160bc8f7c8f3eba533a83c
Pekaizilla <duclee.banh@gmail.com>
2024-12-19 15:28:23 +0100

    Changed some details

--- components/Header.vue
+++ components/Header.vue
@@ -1,25 +1,28 @@
 <template>
     <header>
-        <img class="header-logo" src="/images/Logo.png" alt="Logo de l'entreprise" />
+        <NuxtLink class="header-logo" to="/">
+            <img class="header-logo-image" src="/images/svg/logo_dark.svg" alt="Logo de l'entreprise" />
+        </NuxtLink>
         <nav class="desktop-nav">
             <ul>
                 <li class="link-typo">
                     <NuxtLink class="header-link text-nowrap" to="/">Accueil</NuxtLink>
                 </li>
                 <li class="link-typo">
-                    <NuxtLink class="header-link text-nowrap" to="/services">Mes Services</NuxtLink>
+                    <NuxtLink class="header-link text-nowrap" to="/services">Services</NuxtLink>
                 </li>
                 <li class="link-typo">
                     <NuxtLink class="header-link text-nowrap" to="/about">Qui suis-je ?</NuxtLink>
                 </li>
                 <li class="link-typo">
-                    <NuxtLink class="header-link text-nowrap" to="/news">Mes Actualités</NuxtLink>
+                    <NuxtLink class="header-link text-nowrap" to="/news">Actualités</NuxtLink>
                 </li>
                 <li class="link-typo">
-                    <NuxtLink class="header-link text-nowrap" to="/contact">Me Contacter</NuxtLink>
+                    <NuxtLink class="header-link text-nowrap" to="/contact">Contact</NuxtLink>
                 </li>
             </ul>
         </nav>
+        <Button class="header-cta" buttonType="redirect" text="Contactez nous" to="/contact" buttonClass="button-typo-large" />
     </header>
 </template>
 
@@ -30,25 +33,32 @@
         flex-flow: row nowrap;
         justify-content: space-between;
         align-items: center;
-        gap: 16px;
+        gap: 20px;
         width: 100%;
         margin: 0 auto;
         min-width: var(--width-min);
         max-width: var(--width-max);
         padding-inline: 16px;
         box-sizing: border-box;
-        background-color: var(--color-primary-200);
+        background-color: var(--color-main-black);
         z-index: 999;
+        color: var(--color-main-white);
     }
 
     .header-logo {
-        object-fit: contain;
         height: 80px;
         max-width: 50%;
     }
 
+    .header-logo-image {
+        object-fit: contain;
+        height: 100%;
+    }
+
     .desktop-nav {
         flex: 1;
+        padding: 12px 0;
+        border-radius: 5px;
         max-width: 766px;
     }
 
@@ -58,14 +68,14 @@
         display: flex;
         flex-flow: row nowrap;
         align-items: center;
-        justify-content: space-between;
+        justify-content: space-evenly;
         list-style: none;
         text-decoration: none;
     }
 
     .header-link {
         text-decoration: none;
-        color: var(--color-main-black);
+        color: var(--color-main-white);
     }
 
     .header-link:hover {
@@ -78,6 +88,9 @@
             display: none;
         }
         
+        .header-cta {
+            display: none;
+        }
     }
 
 </style>
